b21c5ad08bc0348ff39dd976a6d1cba4b9329437
BarbarianN <88117334+BarbarianN@users.noreply.github.com>
2021-07-29 00:20:03 -0300

    ex 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const Titulo = (promp) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <h1>{promp.dtl}</h1>
+  )
 }
 
-export default App;
+const Parts = (promp) => {
+  const {part} = promp
+  const {num1} = promp
+  return (
+    <p>
+        {part} {num1}
+      </p>
+  )
+}
+
+const Total = (promp) => {
+  const {e1} = promp
+  const {e2} = promp
+  const {e3} = promp
+  return (
+    <p>Number of exercises {e1 + e2 + e3}</p>
+  )
+}
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Titulo dtl={course}/>
+      <Parts part={part1} num1={exercises1} />
+      <Parts part={part2} num1={exercises2} />
+      <Parts part={part3} num1={exercises3} />
+      <Total e1={exercises1} e2={exercises2} e3={exercises3} />
+    </div>
+  )
+}
+
+export default App
